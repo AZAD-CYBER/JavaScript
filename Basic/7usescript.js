0176db0a4628bd7c082915d10953f3557e42679b
Azad Ansari <79094390+AZAD-CYBER@users.noreply.github.com>
2023-03-02 22:37:59 +0530

    Datatype_number

---
+++ Basic/7usescript.js
@@ -0,0 +1,8 @@
+/*
+Quite soon we’re going to learn functions (a way to group commands), so let’s note in advance that "use strict" can be put at the beginning of a function. Doing that enables strict mode in that function only. But usually people use it for the whole script.
+*/
+// Ensure that “use strict” is at the top
+"use strict";
+var a = 12;
+var b = 13;
+console.log(a + b);
---
+++ Basic/8datatype.js
@@ -0,0 +1,21 @@
+/*
+Data types
+
+A value in JavaScript is always of a certain type. For example, a string or a number.
+
+There are eight basic data types in JavaScript
+*/
+
+// Number
+let n = 123;
+n = 12.345;
+console.log(n);
+
+// Infinity:-Infinity represents the mathematical Infinity ∞. It is a special value that’s greater than any number.
+console.log(1 / 0);
+
+// NaN represents a computational error. It is a result of an incorrect or an undefined mathematical operation, for instance:
+console.log("one" / 2);
+
+// it give output
+console.log("1" / 2);
